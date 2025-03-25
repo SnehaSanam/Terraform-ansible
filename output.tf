d0dbd70f00722db920a522be4f720b6f983edcd3
SnehaSanam <snehassanam@gmail.com>
2025-03-26 02:33:20 +1100

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
 output "ec2_public_ip"{
 value = [
     for instance in  aws_instance.my_instance : {
-        name = instance.tags.name
+        name = instance.tags.Name
         public_ip = instance.public_ip
     }
   ]
